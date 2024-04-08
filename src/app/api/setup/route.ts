54c9d9339584b5f00301092dcb9695cb347badba
Artem <arlaptiev@gmail.com>
2024-04-07 22:45:49 -0400

    file-upload

--- src/app/api/setup/route.ts
+++ src/app/api/setup/route.ts
@@ -1,5 +1,7 @@
 // copied to src/app/_components/createIndex.ts on github.com:sundai-club/reliable-bots
 import { NextResponse } from 'next/server'
+import path from "path";
+import { writeFile } from "fs/promises";
 import { PineconeClient } from '@pinecone-database/pinecone'
 import { TextLoader } from 'langchain/document_loaders/fs/text'
 import { DirectoryLoader } from 'langchain/document_loaders/fs/directory'
@@ -13,33 +15,82 @@ import {
 // const indexName = `index-${crypto.randomBytes(4).toString('hex')}`;
 // import { indexName } from '../../../config'
 
-export async function POST(request) {
-  const { indexName, filePath } = await request.json();
-  // const loader = new DirectoryLoader('./documents', {
-  //   ".txt": (path) => new TextLoader(path),
-  //   ".md": (path) => new TextLoader(path),
-  //   ".pdf": (path) => new PDFLoader(path)
-  // })
-  const loader = new PDFLoader(filePath)
+export const POST = async (req, res) => {
+  const formData = await req.formData();
+  console.log(formData)
 
-  const docs = await loader.load()
-  const vectorDimensions = 1536
-
-  const client = new PineconeClient()
-  await client.init({
-    apiKey: process.env.PINECONE_API_KEY || '',
-    environment: process.env.PINECONE_ENVIRONMENT || ''
-  })
-
-  try {
-    await createPineconeIndex(client, indexName, vectorDimensions)
-    await updatePinecone(client, indexName, docs)
-    console.log('pineconeIndexName: ', indexName)
-  } catch (err) {
-    console.log('error: ', err)
+  const file = formData.get("file");
+  console.log(file)
+  if (!file) {
+    return NextResponse.json({ error: "No files received." }, { status: 400 });
   }
 
-  return NextResponse.json({
-    data: 'successfully created index and loaded data into pinecone...'
-  })
-}
+  const buffer = Buffer.from(await file.arrayBuffer());
+  const filename =  file.name.replaceAll(" ", "_");
+  try {
+    
+    await writeFile(
+      path.join(process.cwd(), "public/assets/" + filename),
+      buffer,
+      { flag: 'w+' }
+    );
+
+
+    // Load Embedding  to Prisma
+    const loader = new PDFLoader(filePath)
+
+    const docs = await loader.load()
+    const vectorDimensions = 1536
+
+    const client = new PineconeClient()
+    await client.init({
+      apiKey: process.env.PINECONE_API_KEY || '',
+      environment: process.env.PINECONE_ENVIRONMENT || ''
+    })
+
+    try {
+      await createPineconeIndex(client, indexName, vectorDimensions)
+      await updatePinecone(client, indexName, docs)
+      console.log('pineconeIndexName: ', indexName)
+    } catch (err) {
+      console.log('error: ', err)
+    }
+    
+    return NextResponse.json({ Message: "Success", status: 201 });
+  } catch (error) {
+    console.log("Error occured ", error);
+    return NextResponse.json({ Message: "Failed", status: 500 });
+  }
+};
+
+// export async function POST(req, res) {
+//   // const { indexName, filePath } = await request.json();
+//   // const loader = new DirectoryLoader('./documents', {
+//   //   ".txt": (path) => new TextLoader(path),
+//   //   ".md": (path) => new TextLoader(path),
+//   //   ".pdf": (path) => new PDFLoader(path)
+//   // })
+
+//   // const loader = new PDFLoader(filePath)
+
+//   // const docs = await loader.load()
+//   // const vectorDimensions = 1536
+
+//   // const client = new PineconeClient()
+//   // await client.init({
+//   //   apiKey: process.env.PINECONE_API_KEY || '',
+//   //   environment: process.env.PINECONE_ENVIRONMENT || ''
+//   // })
+
+//   // try {
+//   //   await createPineconeIndex(client, indexName, vectorDimensions)
+//   //   await updatePinecone(client, indexName, docs)
+//   //   console.log('pineconeIndexName: ', indexName)
+//   // } catch (err) {
+//   //   console.log('error: ', err)
+//   // }
+
+//   // return NextResponse.json({
+//   //   data: 'successfully created index and loaded data into pinecone...'
+//   // })
+// }
