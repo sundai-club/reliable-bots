728d271c4b8626806a0063e9d9ba8164cfd4cafe
leaps <lmtejedor@gmail.com>
2024-04-08 22:09:30 -0400

    Add Typescript rule overrides for Deployment

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -34,6 +34,22 @@ const config = {
         }
       }
     ]
-  }
-}
+  },
+  "overrides": [
+    {
+      "files": ["./src/app/**/*.[tj]sx"],
+      "rules": {
+        "@typescript-eslint/prefer-optional-chain": "off",
+        "@typescript-eslint/no-unsafe-assignment": "off",
+        "@typescript-eslint/no-unsafe-call": "off",
+        "@typescript-eslint/no-unsafe-member-access": "off",
+        "@typescript-eslint/no-unsafe-argument": "off",
+        "@typescript-eslint/no-floating-promises": "off",
+        "@typescript-eslint/prefer-nullish-coalescing": "off",
+        "react/no-unescaped-entities": "off"
+      }
+    }
+  ]
+};
+
 module.exports = config;
