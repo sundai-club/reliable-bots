2c3ef71435fb3bd4e95d45f3482d7f55179ca1e9
leaps <lmtejedor@gmail.com>
2024-04-09 01:48:28 -0400

    Change the way bots are loaded into the Chat interface

--- src/app/[template_slug]/[bot_id]/page.tsx
+++ src/app/[template_slug]/[bot_id]/page.tsx
@@ -1,8 +1,5 @@
 import { headers } from "next/headers";
 import Chat from "~/app/_components/Chat";
-import MobileSiderbar from "~/app/_components/MobileSidebar";
-import Sidebar from "~/app/_components/Sidebar";
-import useAnalytics from "~/app/_hooks/useAnalytics";
 import { api } from "~/trpc/server";
 
 export default async function ChatBot() {
@@ -10,20 +7,19 @@ export default async function ChatBot() {
   const referer = headersList.get("referer");
   let botIdNum;
 
-  if (referer) {
-    const url = new URL(referer);
-    const pathname = url.pathname;
-    const matches = pathname.match(/\/bots\/(\d+)/);
-    if (matches && matches[1]) {
-      botIdNum = parseInt(matches[1], 10);
-    }
-  }
 
-  let bot = null;
-  if (botIdNum !== undefined) {
-    bot = await api.post.getBotById({ id: botIdNum });
-    console.log(bot);
+  const url = new URL(referer!);
+  const pathname = url.pathname;
+  const matches = pathname.match(/\/bots\/(\d+)/);
+  if (matches && matches[1]) {
+    botIdNum = parseInt(matches[1], 10);
   }
+  
+
+
+  const bot = await api.post.getBotById({ id: botIdNum! });
+  console.log(bot);
+  
 
   return (
     <main className="overflow-hidden w-full h-screen relative flex">
