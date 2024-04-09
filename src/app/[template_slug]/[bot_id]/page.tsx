bddd2bce0976fa99c5bcf4f7035478a1b829d3d1
leaps <lmtejedor@gmail.com>
2024-04-09 02:06:57 -0400

    New method for getting the bot id

--- src/app/[template_slug]/[bot_id]/page.tsx
+++ src/app/[template_slug]/[bot_id]/page.tsx
@@ -2,24 +2,32 @@ import { headers } from "next/headers";
 import Chat from "~/app/_components/Chat";
 import { api } from "~/trpc/server";
 
-export default async function ChatBot() {
-  const headersList = headers();
-  const referer = headersList.get("referer");
-  let botIdNum;
 
+export default async function ChatBot({ params }: { params: any }) {
+  //const headersList = headers();
+  //const referer = headersList.get("referer");
+  //let botIdNum;
 
-  const url = new URL(referer!);
-  const pathname = url.pathname;
-  const matches = pathname.match(/\/bots\/(\d+)/);
-  if (matches && matches[1]) {
-    botIdNum = parseInt(matches[1], 10);
+  // THE ISSUE HERE: this isn't getting the URL in production
+  /*
+  if (referer) {
+    const url = new URL(referer);
+    const pathname = url.pathname;
+    const matches = pathname.match(/\/bots\/(\d+)/);
+    if (matches && matches[1]) {
+      botIdNum = parseInt(matches[1], 10);
+    }
   }
-  
+  */
+  console.log(params['bot_id'])
 
+  const botIdNum = parseInt(params['bot_id'])
 
-  const bot = await api.post.getBotById({ id: botIdNum! });
-  console.log(bot);
-  
+  let bot = null;
+  if (botIdNum !== undefined) {
+    bot = await api.post.getBotById({ id: botIdNum });
+    console.log(bot);
+  }
 
   return (
     <main className="overflow-hidden w-full h-screen relative flex">
